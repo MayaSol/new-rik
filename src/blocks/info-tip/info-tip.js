e09a4bfb5422fc477e59ca539a2bc6d577d95b0c
Maya Soloveva <maya.soloveva2015@yandex.ru>
2022-10-12 19:08:37 +0300

    delivery page

---
+++ src/blocks/info-tip/info-tip.js
@@ -0,0 +1,26 @@
+const ready = require('../../js/utils/documentReady.js');
+import tippy from 'tippy.js';
+
+ready(function() {
+  var infoTips = document.querySelectorAll('.info-tip');
+
+
+  for (var info of infoTips) {
+    var contentId = info.dataset.content;
+    var placement = 'top';
+    if (info.classList.contains('info-tip--right')) {
+      placement = 'right';
+    }
+    if (contentId) {
+      tippy(info, {
+        content: contentId,
+        theme: 'info-tip',
+        allowHTML: true,
+        interactive: true,
+        placement: placement,
+        maxWidth: 468,
+        trigger: 'click'
+      })
+    }
+  }
+});
